07a1c7dc38c133c03ef190c5602bf5137e4ecbb2
incredible92 <sulymanabdullahi@gmail.com>
2020-10-12 11:03:12 +0100

    exercise2

--- src/App.js
+++ src/App.js
@@ -3,26 +3,34 @@ import React, { useState } from "react";
 export const generateId = () => Math.random().toString(36).substring(2, 6);
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas', phoneNumber: '234-43-802245' },
+    { id: 2, name: 'Abdullahi Sulyman', phoneNumber: '08060131846' },
+    { id: 3, name: 'Sheedah Albayan', phoneNumber: '08172285514' },
+    { id: 4, name: 'Samsuit Dare', phoneNumber: '07085842564' }
+  ]);
   const [newName, setNewName] = useState("");
+  
 
-  const addName = (e) => {
+  const [newNumber, setNewNumber] = useState('')
+
+  const addNewContact = (e) => {
     e.preventDefault();
 
-    if (
-      Object.keys(persons).some((personId) => {
-        const person = persons[personId];
-        return person.name.toLowerCase() === newName.trim().toLowerCase();
-      })
-    ) {
-      setNewName("");
-      return window.alert(`${newName} is already added to phonebook`);
-    }
+    if (persons.some(person => person.name.toLowerCase().trim() === newName.trim().toLowerCase())) {
+      setNewName('')
+      setNewNumber('')
 
-    const newInput = {};
-    newInput[generateId()] = {
+      return window.alert(`${newName} has already been added to the phonebook `)
+    }
+    
+    
+    const newInput = {
+      id: generateId(),
       name: newName,
-    };
+      phoneNumber: newNumber
+    }	    
+
     setPersons({
       ...persons,
       ...newInput,
@@ -32,22 +40,25 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={(e) => addName(e)}>
+      <form onSubmit={(e) => addNewContact(e)}>
         <div>
-          name:{" "}
+          name:
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
+
+        <div>
+          number:
+          <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <p>
-        {Object.keys(persons).map((personId) => {
-          const person = persons[personId];
-          return <p key={personId}>{person.name}</p>;
-        })}
-      </p>
+      <p>{
+      persons.map(person => <p key={person.id}><span>{person.name````}</span> &nbsp; <span>{person.phoneNumber}</span></p>)}
+</p>
     </div>
   );
 };
